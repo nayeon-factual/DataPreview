76a1e16342b8ea297a7c10abc2e416aa5e3cdf1a
nayeon-factual <nayeon@factual.com>
2014-08-11 14:34:13 -0700

    dataResultsGrid is now sortable by column

--- js/DataPreview_Table.js
+++ js/DataPreview_Table.js
@@ -1,4 +1,3 @@
-var grid;
 var columns = []; 
 var readCall = "";
 
@@ -8,17 +7,20 @@ enableColumnReorder: false
 };
 
 function populateDataGridHeading(fieldsDataObject){
+    if(fieldsDataObject.label!='Address Extended'&&fieldsDataObject.label!='Category IDs'&&fieldsDataObject.label!='Chain ID'){
     var gridHeading = {};
     gridHeading['id']=fieldsDataObject.name;
     gridHeading['name']=fieldsDataObject.label;
     gridHeading['field']=fieldsDataObject.name;
+    gridHeading['sortable']=true;
     columns.push(gridHeading);
+    }
 }
 
 function makeReadCall(){
     var initReadCall = URL.replace('www.factual.com/data','api.v3.factual.com');
     initReadCall += '&limit=50&KEY='+key;
-    readCall = initReadCall.replace('places#','places?');
+    readCall = initReadCall.replace(table_id+'#',table_id+'?');
 //    console.log('gridColumns '+JSON.stringify(columns));
     $.get(readCall).done(function(data){
         var readResults = data.response.data;
@@ -28,7 +30,8 @@ function makeReadCall(){
             for(c=0; c<columns.length; c++){
                 var columnsField = columns[c].field;
                 dataArray[l][columnsField]=readResults[l][columnsField];
-            }            
+            }
+            dataArray[l]['id']=readResults[l]['factual_id']
         }
         createGrid(dataArray);
     })
@@ -36,6 +39,22 @@ function makeReadCall(){
 
 function createGrid(dataArray) {
   $(function () {
-    grid = new Slick.Grid(".dataResultsGrid", dataArray, columns, options);
+//      console.log(dataArray);
+      var dataView = new Slick.Data.DataView();
+      var grid = new Slick.Grid(".dataResultsGrid", dataView, columns, options);
+      
+      dataView.onRowsChanged.subscribe(function(e,args) {
+          grid.invalidateRows(args.rows);
+          grid.render();
+      });
+      dataView.setItems(dataArray);
+      
+      grid.onSort.subscribe(function(e, args) {
+          var comparer = function(a, b) {
+              return (a[args.sortCol.field] > b[args.sortCol.field]) ? 1 : -1;
+          }
+      dataView.sort(comparer, args.sortAsc);
+      });
+      
   })
 }
