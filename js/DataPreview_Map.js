ed1fae9588d36f1cdc80a90ed6c85caa294862ed
nayeon-factual <nayeon@factual.com>
2014-08-21 10:23:25 -0700

    marker opens factual id page on click

--- js/DataPreview_Map.js
+++ js/DataPreview_Map.js
@@ -7,7 +7,7 @@ options = {
 };
 
 oauth = OAuth(options);
-    
+
 L.mapbox.accessToken = 'pk.eyJ1IjoibmF5ZW9uLWZhY3R1YWwiLCJhIjoiWTF1Nzk0SSJ9.HkA7gvIL9dZXrJ6Yy9gJog';
 var map = L.mapbox.map('dataResultsMap', 'nayeon-factual.ijioib62')
   .setView([37.8, -96], 4);
@@ -17,13 +17,26 @@ var markers = new L.MarkerClusterGroup();
 
 function initMap(coorSet){
   	markers.clearLayers();
-
-  	for (var i = 0; i < coorSet.length; i++) {
+   	for (var i = 0; i < coorSet.length; i++) {
       var a = coorSet[i];
-      var marker = L.marker(new L.LatLng(a[0], a[1]));
+      if(a[2]!=undefined){
+      	var f_id = a[2];
+      }else{
+      	var f_id = "";
+      }
+      var marker = L.marker(new L.LatLng(a[0], a[1]), {
+      	// icon:
+      	url:"http://www.factual.com/"+f_id
+      });
       markers.addLayer(marker);
   	}
  	 map.addLayer(markers);
+
+ 	markers.on('click', function(e){
+  		// console.log(e);
+  		var openURL = e.layer.options.url;
+ 		window.open(openURL);
+ 	});
  
  	 map.fitBounds(markers.getBounds());
 }
--- js/MapGeojson.js
+++ js/MapGeojson.js
@@ -43,8 +43,10 @@ function getCoordinates(readResults){
       if(readResults[l]['latitude']!=undefined&&readResults[l]['longitude']!=undefined){
         var lat = readResults[l]['latitude'];
         var lng = readResults[l]['longitude'];
+        var id = readResults[l]['factual_id'];
           coor[0]=lat;
           coor[1]=lng;
+          coor[2]=id;
           coorSet.push(coor);
       }
     }
